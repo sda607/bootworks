9c2c3bd1b91871e1b059dc99ebedacb4548a6011
sda607 <sda607@naver.com>
2022-09-22 16:07:49 +0900

    Repository

---
+++ springJPA/src/main/java/com/boot/persistence/BoardRepository.java
@@ -0,0 +1,23 @@
+package com.boot.persistence;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.boot.domain.Board;
+
+public interface BoardRepository extends CrudRepository<Board, Long>{
+
+	//쿼리 메소드 - 글 제목을 검색
+	List<Board> findByTitle(String searchKeyword);
+	
+	//특정 단어가 포함된 목록 검색(Containing - Like 역할)
+	List<Board> findByContentContaining(String searchKeyWord);
+	
+	//제목 또는 내용에 특정 단어가 포함된 목록 검색
+	List<Board> findByTitleContainingOrContentContaining(String title, String content);
+	
+	//글 제목이 특정 단어가 포함된 글 목록을 내림차순으로 조회
+	List<Board> findByTitleContainingOrderBySeqDesc(String searchKeyword);
+	
+}
